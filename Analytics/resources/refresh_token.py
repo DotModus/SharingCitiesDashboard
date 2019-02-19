279ac8ca2a9f90b3e111b0f3508330ce9a7fd677
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-19 11:29:01 +0200

    updated docstring for token_refresh.py

--- Analytics/resources/refresh_token.py
+++ Analytics/resources/refresh_token.py
@@ -2,6 +2,11 @@ from flask_restful import Resource
 from flask_jwt_extended import  jwt_refresh_token_required, get_jwt_identity, create_access_token
 
 class TokenRefresh(Resource):
+    """API that creates a new valid access JWT for the requesting user
+        :required: A valid refresh JWT in the Authorization Header in the format - Bearer <JWT>
+        :return: The user's corresponding access JWT
+        :rtype: JSON
+    """
     @jwt_refresh_token_required
     def post(self):
         current_user = get_jwt_identity() # extracts the users identity from the refresh token
